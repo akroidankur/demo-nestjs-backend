921a1afe494769896b9659a3efe2169d5c36fdfe
akroidankur <developerankurhazarika@gmail.com>
2025-01-14 16:11:07 +0530

    mongofix

--- src/mongo.module.ts
+++ src/mongo.module.ts
@@ -6,9 +6,16 @@ import { MongooseModule } from '@nestjs/mongoose';
     imports: [
         MongooseModule.forRootAsync({
             imports: [ConfigModule],
-            useFactory: async (configService: ConfigService) => ({
-              uri: configService.get<string>('MONGO_CONNECT'),
-            }),
+
+            useFactory: async (configService: ConfigService) => {
+              const uri = configService.get<string>('MONGO_CONNECT');
+              console.log('Mongo URI:', uri);
+              return { uri };
+            },
+            
+            // useFactory: async (configService: ConfigService) => ({
+            //   uri: configService.get<string>('MONGO_CONNECT'),
+            // }),
             inject: [ConfigService],
           }),
     ]
