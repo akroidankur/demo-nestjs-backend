124672c3fa32f1eaeca275df0aaebe3c4ad832f6
akroidankur <developerankurhazarika@gmail.com>
2025-01-14 20:34:58 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
+  config();
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const allowedOrigins = [
+      'http://localhost:4200', // for local
+      'https://demo-nestjs-backend.onrender.com',  // for stage
+    ];
+
+    const origin = req.headers.origin as string;
+
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+
+  app.enableCors({
+    origin: false,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT);
 }
+
 bootstrap();
